0316482cf5340187950af0c9bc578f1e802c799b
Shehara_r <ranathisarashehara01@gmail.com>
2022-08-10 23:06:36 +0530

    borders?

--- src/Components/CountryMain.tsx
+++ src/Components/CountryMain.tsx
@@ -1,25 +1,26 @@
 import axios from 'axios';
+import { iteratorSymbol } from 'immer/dist/internal';
 import React, { useEffect, useState } from 'react';
-import { ObjectFlags } from 'typescript';
-import { isNativeError } from 'util/types';
+import { brotliDecompress } from 'zlib';
 
 type Props = { item: Country };
 
 const CountryMain = ({ item }: Props) => {
-  const [cName, setCName] = useState('');
+  const [country, setCountry] = useState<Country[]>();
+  const [borderCountry, setBorderCountry] = useState<string>();
 
-  //   let cData = Promise.all(
-  //     item?.borders.map(async (border) => {
-  //       const res = await axios.get(
-  //         `https://restcountries.com/v3.1/alpha/${border}`
-  //       );
-  //       return res.data;
-  //       //   console.log(res.data);
-  //     })
-  //   );
+  // const getBorder = async (border: string) => {
+  //   await axios
+  //     .get(`https://restcountries.com/v3.1/alpha/${border?.toLocaleLowerCase()}`)
+  //     .then((res) => res.data)
+  //     .then((result) => setBorderCountry(result.name.common)).catch(error => console.log(error)
+  //     )
+
+  //   return borderCountry;
+  // };
 
   return (
-    <div className='mx-[10vw] mt-8'>
+    <div className='mx-[10vw] mt-8 mb-10'>
       <img src={item.flags.png} alt={item.name.common} className='w-full' />
       <div className='text-sm'>
         <h1 className='font-bold text-lg mt-6'>{item.name.common}</h1>
@@ -65,7 +66,18 @@ const CountryMain = ({ item }: Props) => {
           </span>
         </p>
       </div>
-      <div>{/* {item.borders.} */}</div>
+      <p className='attribute text-sm mt-6'>Border countries: </p>
+      <div className='text-xs font-semibold flex gap-2 items-center mt-2 flex-wrap'>
+        {item.borders.length > 0 ? (
+          item.borders.map((b, i) => (
+            <div key={i} className=''>
+              <h1 className='bg-white shadow-md rounded-sm px-6 py-1'>{b}</h1>
+            </div>
+          ))
+        ) : (
+          <p>-</p>
+        )}
+      </div>
     </div>
   );
 };
